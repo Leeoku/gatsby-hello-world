30f0ce9e9d3536bbc07130f3c31fcbbef6b3333a
Leeoku <leeoku@gmail.com>
2021-01-09 15:45:00 -0500

    FIRST COMMIT

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">Contact</Link>
+      <p>WHAT A WORLD</p>
+      
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+    
+  )
 }
